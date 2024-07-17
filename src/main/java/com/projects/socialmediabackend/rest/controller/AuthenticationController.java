2d5199b907c074110efa40a9e6071d91782b0e5d
janetheboss <101011386+janetheboss@users.noreply.github.com>
2024-07-17 14:19:32 +0300

    LogInService implemented

--- src/main/java/com/projects/socialmediabackend/rest/controller/AuthenticationController.java
+++ src/main/java/com/projects/socialmediabackend/rest/controller/AuthenticationController.java
@@ -1,5 +1,8 @@
 package com.projects.socialmediabackend.rest.controller;
 
+import com.projects.socialmediabackend.api.login.UserLogInOperation;
+import com.projects.socialmediabackend.api.login.UserLogInRequest;
+import com.projects.socialmediabackend.api.login.UserLogInResponse;
 import com.projects.socialmediabackend.api.user.register.UserRegisterOperation;
 import com.projects.socialmediabackend.api.user.register.UserRegisterRequest;
 import com.projects.socialmediabackend.api.user.register.UserRegisterResponse;
@@ -15,9 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/auth/users")
 public class AuthenticationController {
     private final UserRegisterOperation userRegisterOperation;
+    private final UserLogInOperation userLogInOperation;
 
     @PostMapping
     public ResponseEntity<UserRegisterResponse> register(@RequestBody UserRegisterRequest request) {
         return ResponseEntity.ok(userRegisterOperation.process(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserLogInResponse> login (@RequestBody UserLogInRequest request) {
+        return ResponseEntity.ok(userLogInOperation.process(request));
     }
 }
